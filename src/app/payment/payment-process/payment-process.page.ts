fd50db076f4cd4c8525e846ca0c3fab01439f1b0
nadinethsyaa <58587724+nadinethsyaa@users.noreply.github.com>
2020-12-02 23:52:20 +0700

    payment method process

--- src/app/payment/payment-process/payment-process.page.ts
+++ src/app/payment/payment-process/payment-process.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AlertController} from '@ionic/angular';
+import {ActivatedRoute} from '@angular/router';
+import {BankService} from '../../services/bank.service';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-payment-process',
@@ -8,7 +11,33 @@ import {AlertController} from '@ionic/angular';
 })
 export class PaymentProcessPage implements OnInit {
 
-  constructor(public alertController: AlertController) { }
+  bankID: string;
+  nama: any;
+  logo: any;
+  bankItems: any;
+  constructor(public alertController: AlertController,
+              private banksService: BankService,
+              private activatedRoute: ActivatedRoute)
+  { }
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('id')) {
+        return;
+      }
+      this.bankID = paramMap.get('id');
+      console.log(this.bankID);
+      this.banksService.getDetailBank(this.bankID).snapshotChanges().pipe(
+          map(changes =>
+              changes.map(c => ({key: c.payload.key, ...c.payload.val()}))
+          )
+      ).subscribe(data => {
+        this.bankItems = data;
+        console.log(this.bankItems);
+      });
+    });
+  }
+
   async presentAlert() {
     const alert = await this.alertController.create({
       message: 'Berhasil disalin',
@@ -16,8 +45,6 @@ export class PaymentProcessPage implements OnInit {
     });
 
     await alert.present();
-  }
-  ngOnInit() {
   }
 
 }
