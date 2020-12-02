a6b307523c3d79621de564bd25126d8ec3ef2bec
nadinethsyaa <58587724+nadinethsyaa@users.noreply.github.com>
2020-12-02 19:16:25 +0700

    alamat

---
+++ src/app/main/keranjang/alamat/alamat.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alamat',
+  templateUrl: './alamat.page.html',
+  styleUrls: ['./alamat.page.scss'],
+})
+export class AlamatPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
